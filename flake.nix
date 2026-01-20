31631c599341ae1655d9584b18100ebd69b58f3a
Abiria <oro3673@gmail.com>
2026-01-20 05:15:34 -0500

    wrangler nix

--- flake.nix
+++ flake.nix
@@ -2,6 +2,10 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    wrangler = {
+      url = "github:emrldnix/wrangler";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = (
@@ -9,6 +13,7 @@
       self,
       nixpkgs,
       flake-utils,
+      wrangler,
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
@@ -17,12 +22,17 @@
       in
       with pkgs;
       {
+        nix.settings = {
+          substituters = [ "https://wrangler.cachix.org" ];
+          trusted-public-keys = [ "wrangler.cachix.org-1:N/FIcG2qBQcolSpklb2IMDbsfjZKWg+ctxx0mSMXdSs=" ];
+        };
+
         devShells.default = mkShell {
           packages = [
             nodejs
             pnpm
             litecli
-            wrangler
+            wrangler.packages.${system}.wrangler
           ];
         };
       }
